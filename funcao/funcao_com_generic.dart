fd878888720692642776f31a6077784a351ff402
sarahmelo <sarahcbdg@gmail.com>
2021-06-10 21:55:10 -0300

    function generics

--- funcao/funcao_com_generic.dart
+++ funcao/funcao_com_generic.dart
@@ -3,7 +3,16 @@ Object segundoElementoV1(List lista) {
   return lista.length >= 2 ? lista[1] : null;
 }
 
+E segundoElementoV2<E>(List lista) {
+  return lista.length >= 2 ? lista[1] : null;
+}
+
 main() {
   var lista = [1, 2, 3, 4, 5, 6, 7];
+  var listas = ['Banana', 'Maçã', 'Abacate', 'Uva'];
+
   print(segundoElementoV1(lista));
+
+  print(segundoElementoV2<int>(lista));
+  print(segundoElementoV2<String>(listas));
 }
